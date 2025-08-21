e7818e455b604b464da80fb15e189992fea8a5b6
unknown <jasujarishu1@gmail.com>
2025-08-21 11:34:15 +0530

    climb stairs by recursion p=70

---
+++ 79_climb_stairs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int climbstairs(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    
+    return climbstairs(n-1)+climbstairs(n-2);
+}
+int main(){
+    int n=4;
+    int ans=climbstairs(n);
+    cout<<ans<<endl;
+}
